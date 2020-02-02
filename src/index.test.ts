6feaa2b9ac34dd2b81520cd6fdb489b836e438dc
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2020-02-02 14:22:15 -0600

    Finish up test for useToggle

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,13 +10,27 @@ import {
 } from './index';
 
 test('useToggle should toggle between `true` and `false`', () => {
-  const { result } = renderHook(() => useToggle(true))
-
+  const { result } = renderHook(() => useToggle(true));
   expect(result.current[0]).toBe(true);
-
-  act(() => {
-    result.current[1]();
-  });
-
-  expect(result.current[0]).toBe(false);
+  function testToggle(whichFunc: 'toggle' | 'setToTrue' | 'setToFalse', startValue: boolean, endValue: boolean) {
+    let [ value, toggle, setToTrue, setToFalse ] = result.current;
+    expect(value).toBe(startValue);
+    act(() => {
+      if (whichFunc === 'toggle') {
+        toggle();
+      } else if (whichFunc === 'setToTrue') {
+        setToTrue();
+      } else if (whichFunc === 'setToFalse') {
+        setToFalse();
+      }
+    });
+    [ value ] = result.current;
+    expect(value).toBe(endValue);
+  }
+  testToggle('toggle', true, false);
+  testToggle('toggle', false, true);
+  testToggle('setToFalse', true, false);
+  testToggle('setToFalse', false, false);
+  testToggle('setToTrue', false, true);
+  testToggle('setToTrue', true, true);
 });
