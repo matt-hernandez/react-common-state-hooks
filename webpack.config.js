3b2cb41e081f15d6900af565411c3b5be77fb68d
matthernandez <matt.hernandez@idean.com>
2020-02-02 10:30:02 -0600

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'react-common-state-hooks': './src/index.ts'
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'ReactCommonStateHooks',
+    umdNamedDefine: true
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
